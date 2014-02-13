0aa61030bbc20dfea985fe31a634a04fd44b05b7
Steve Purcell <steve@sanityinc.com>
2014-02-13 17:27:56 +0000

    Add a bin-packing algorithm and use it to pack retrieved songs

--- SpotifyPack.hs
+++ SpotifyPack.hs
@@ -8,6 +8,8 @@ import qualified Data.Aeson.Types as T
 import qualified Data.ByteString.Lazy.Char8 as BSL
 import Control.Applicative ((<$>), (<*>))
 import Control.Monad (mzero)
+import Data.List (minimumBy)
+import Data.Maybe (fromMaybe)
 
 data Song = Song { label :: String, duration :: Float }
           deriving (Show, Eq, Ord)
@@ -33,3 +35,45 @@ parseResponse resp =
     do result <- decode resp
        flip T.parseMaybe result $ \obj ->
            parseJSON =<< (obj .: "tracks")
+
+
+-- Bin-packing "first-fit" algorithm, https://en.wikipedia.org/wiki/Bin_packing_problem
+
+data Bin a b = Bin { items :: [a], available :: b } deriving Eq
+
+packItem :: Num b => Ord b => [Bin a b] -> b -> b -> a -> [Bin a b]
+packItem []     binSize cost item = [ Bin { items = [item], available = binSize - cost } ]
+packItem (b:bs) binSize cost item
+    | cost <= available b = (b { items = items b ++ [item], available = available b - cost }) : bs
+    | otherwise           = b : packItem bs binSize cost item
+
+packAll :: Num b => Ord b => [a] -> (a -> b) -> b -> [Bin a b]
+packAll things getCost limit = foldr packNext [] things
+    where packNext item bins = packItem bins limit (getCost item) item
+
+-- Public interface for packing
+bestPack :: Num b => Ord b => (a -> b) -> b -> [a] -> [a]
+bestPack getCost limit things = items $ minimumBy compareByAvailable $ packAll things getCost limit
+    where compareByAvailable t1 t2 = compare (available t1) (available t2)
+
+
+
+
+-- Tying it all together
+packSongs :: Float -> [Song] -> [Song]
+packSongs = bestPack duration
+
+results :: String -> Float -> IO [Song]
+results term limit = do
+  found <- searchSpotify term
+  return $ packSongs limit $ fromMaybe [] found
+
+totalDuration :: [Song] -> Float
+totalDuration = sum . map duration
+
+{-
+*SpotifyPack> results "Happy" 600
+[Song {label = "Kiss The Stars", duration = 194.131},Song {label = "Chloe", duration = 198.386},Song {label = "Stay Happy There", duration = 207.026}]
+*SpotifyPack> totalDuration `fmap` results "Happy" 600
+599.54297
+-}
